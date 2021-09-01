7ffefa743254d9316d94eb966c83f2581712ddff
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-09-01 09:52:12 +0200

    Try to install through pip

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,18 @@
+setlocal EnableDelayedExpansion
+
 :: configure
 cmake ^
       -H"%SRC_DIR%" ^
       -Bbuild ^
       -GNinja ^
+      -DCMAKE_CXX_COMPILER:STRING=clang-cl ^
       -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_LIBDIR="Library\lib" ^
       -DPYMOD_INSTALL_LIBDIR="..\..\Lib\site-packages" ^
-      -DCMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996 /EHsc" ^
       -DCMAKE_INSTALL_INCLUDEDIR="Library\include" ^
       -DCMAKE_INSTALL_BINDIR="Library\bin" ^
       -DCMAKE_INSTALL_DATADIR="Library" ^
-      -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=true ^
       -DBUILD_SHARED_LIBS=ON ^
       -DINSTALL_DEVEL_HEADERS=OFF ^
       -DENABLE_OPENMP=ON ^
