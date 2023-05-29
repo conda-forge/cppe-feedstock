1e5cfaf9753573f1959969f4f25859d7809b5b05
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2023-05-29 13:35:40 +0200

    Try something adventurous

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,14 +13,8 @@ cmake ^
     -DENABLE_OPENMP:BOOL=ON ^
     -DENABLE_ARCH_FLAGS:BOOL=OFF ^
     -DENABLE_PYTHON_INTERFACE:BOOL=ON ^
-    -DPYMOD_INSTALL_FULLDIR:PATH="Lib\site-packages\cppe" ^
-    -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS:BOOL=ON
+    -DPYMOD_INSTALL_FULLDIR:PATH="Lib\site-packages\cppe"
 if errorlevel 1 exit 1
-
-:: -DCMAKE_INSTALL_LIBDIR:PATH="Library\lib" ^
-:: -DCMAKE_INSTALL_INCLUDEDIR:PATH="Library\include" ^
-:: -DCMAKE_INSTALL_BINDIR:PATH="Library\bin" ^
-:: -DCMAKE_INSTALL_DATADIR:PATH="Library" ^
 
 :: build!
 cmake --build build --config Release --parallel %CPU_COUNT% -- -v -d stats
