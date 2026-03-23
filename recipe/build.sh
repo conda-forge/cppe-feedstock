c472616f0bd718910b991f51316244ca611d79c5
Maximilian Scheurer <11406414+maxscheurer@users.noreply.github.com>
2026-03-23 13:16:48 +0100

    adapt to new build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,32 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-export CXX=$(basename ${CXX})
+export SETUPTOOLS_SCM_PRETEND_VERSION="${PKG_VERSION}"
+export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_FIND_FRAMEWORK:STRING=NEVER -DCMAKE_FIND_APPBUNDLE:STRING=NEVER -DENABLE_OPENMP:BOOL=ON -DENABLE_ARCH_FLAGS:BOOL=OFF -DFETCHCONTENT_FULLY_DISCONNECTED:BOOL=ON -DEigen3_ROOT:PATH=${PREFIX}"
 
-ARCH_ARGS=""
-IS_PYPY=$(${PYTHON} -c "import platform; print(int(platform.python_implementation() == 'PyPy'))")
-
-if [[ $IS_PYPY == 1 ]]; then
-    ARCH_ARGS="-DPython_INCLUDE_DIR=${PREFIX}/include/pypy${PY_VER} ${ARCH_ARGS}"
-fi
-
-# configure
-cmake ${CMAKE_ARGS} ${ARCH_ARGS} \
-    -S"${SRC_DIR}" \
-    -Bbuild \
-    -GNinja \
-    -DCMAKE_BUILD_TYPE:STRING=Release \
-    -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
-    -DCMAKE_CXX_COMPILER:STRING="${CXX}" \
-    -DCMAKE_FIND_FRAMEWORK:STRING=NEVER \
-    -DCMAKE_FIND_APPBUNDLE:STRING=NEVER \
-    -DPython_EXECUTABLE:STRING="${PYTHON}" \
-    -DENABLE_OPENMP:BOOL=ON \
-    -DENABLE_ARCH_FLAGS:BOOL=OFF \
-    -DPYMOD_INSTALL_FULLDIR:PATH="${SP_DIR#$PREFIX/}/cppe"
-
-# build
-cmake --build build --parallel "${CPU_COUNT}" -- -v -d stats
-
-# install
-cmake --build build --target install
+"${PYTHON}" -m pip install . -vv --no-deps --no-build-isolation
